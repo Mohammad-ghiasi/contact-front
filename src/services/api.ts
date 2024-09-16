102d7612c1ebb7495aae20b0fe71b8578f41657d
mohammad <mohammadghiasi005@gmail.com>
2024-09-17 02:32:17 +0330

    front-v-3-1-2

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:3000/',
+  baseURL: 'https://contacts-api-3q2l.vercel.app/',
   withCredentials: true, // Enable credentials
 });
